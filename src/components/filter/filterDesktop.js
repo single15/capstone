6eee4e1ebb774cc13bd480090cb3f6d2ab83b749
single15 <s.ingle@zensar.com>
2022-07-19 12:45:19 +0530

    handle filter categories on page

--- src/components/filter/filterDesktop.js
+++ src/components/filter/filterDesktop.js
@@ -1,16 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import FilterBlock from "components/filter/filterBlock";
 import FilterTag from "components/filter/filterTag";
 import { CATEGORIES_FILTER } from "components/filter/utils";
+import { useParams } from "react-router-dom";
 
 
 const FilterDesktop = (props) => {
     const [selectedFilter, setFilter] = useState();
+    const [categoryOption, setCategoryOption] = useState(CATEGORIES_FILTER);
+    const { category } = useParams();
 
     const setFilterValue = (value) => {
         let items = selectedFilter || [];
         const index = (selectedFilter || []).indexOf(value);
-        if(index > -1) {
+        if (index > -1) {
             items = selectedFilter.filter(f => f !== value);
         } else {
             items = [...items, value];
@@ -19,16 +22,29 @@ const FilterDesktop = (props) => {
         props.setCategory(items);
     }
 
+    useEffect(() => {
+        if (category && category !== 'shop') {
+            let categories = CATEGORIES_FILTER.filter(cat => cat.id === category);
+            setCategoryOption(categories);
+            setFilter([categories[0].value]);
+        } else {
+            setFilter([]);
+            setCategoryOption(CATEGORIES_FILTER);
+        }
+    }, [category]);
+
     return (
         <div className="filter-section-desktop">
             <div className="section-header">
                 Filters
             </div>
             <FilterTag selectedFilter={selectedFilter} clearFilter={() => {
-                setFilter([]);
-                props.setCategory([]);
+                if(categoryOption.length > 1) {
+                    setFilter([]);
+                    props.setCategory([]);
+                }
             }} />
-            <FilterBlock blockLabel={"Categories"} options={CATEGORIES_FILTER} setFilterValue={setFilterValue} selectedFilter={selectedFilter} />
+            <FilterBlock blockLabel={"Categories"} options={categoryOption} setFilterValue={setFilterValue} selectedFilter={selectedFilter} />
         </div>
     )
 }
